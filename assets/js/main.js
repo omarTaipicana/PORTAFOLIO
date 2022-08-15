78ede2ac3849dc28a9918426bbef76cc8a2de8fd
ferTaipi <fernandoparadoja@gmail.com>
2022-08-15 18:32:11 -0500

    agregar

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+const menu=document.querySelector("ul.nav--menu")
+const icon=document.getElementById("menu-icon")
+
+icon.addEventListener("click", e)
+    function e(){
+        menu.classList.toggle("visible")
+    }
